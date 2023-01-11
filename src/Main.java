2e18ee0d43f04f1402a3ca2bf47f2c50b8f1b2cc
vaceslavkameronov <slka@mail.ru>
2023-01-11 21:04:42 +0400

    Решение 2й задачи

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        // task2();
+        task2();
     }
 
     public static void task1() {
@@ -13,4 +13,23 @@ public class Main {
             System.out.println("Установите версию приложения для Android");
         }
     }
-}
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientOS = 0;
+        int clientDeviseYear = 2013;
+        if (clientDeviseYear >=2015) {
+            if ( clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS");
+            } else {
+                System.out.println("Установите версию для Android");
+            }
+            } else if (clientDeviseYear < 2015) {
+        if (clientOS ==0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+                System.out.println("установите облегченную версию приложения для Android  по ссылке");
+            }
+                }
+            }
+        }
+
