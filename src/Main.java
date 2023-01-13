37293e1d68e83234daa03ad86489074ab54bece6
vaceslavkameronov <slka@mail.ru>
2023-01-13 22:01:07 +0400

    Решение 4й задачи

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -34,7 +35,7 @@ public class Main {
     }
     public static void task3() {
         System.out.println("Задача 3");
-        int year = 2023; //инициализация значения года для проверки
+        int year = 2024; //инициализация значения года для проверки
         int signOfLeapYear1 = 4; //инициализация признака високосный год - каждые 4 года
         int signOfLeapYear2 =400; //инициализация признака високосный год - каждые 400 лет
         int signOfLeapYearNon = 100; //инициализация признака не високосный год - каждые 100 лет
@@ -51,5 +52,21 @@ public class Main {
             System.out.println(year + "  год не является високосным");
         }
         }
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95; // инициализация расстояния от офиса до адреса доставки
+        int deliveryOneDay = 20; //инициализация расстояния для доставки за 1 день до 20 км
+        int deliveryTwoDay = 60; //инициализация расстояния для доставки за 2 дня от 20 до 60 км
+        int deliveryThreeDay = 100; //инициализация расстояния для доставки за 3 дня от 60 до 100 км
+        if (deliveryDistance <= deliveryOneDay) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (deliveryDistance <= deliveryTwoDay) {
+            System.out.println("Потребуется дней: " + 2);
+        } else if (deliveryDistance <= deliveryThreeDay) {
+        System.out.println("Потребуется дней: " + 3);
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
 }
 
